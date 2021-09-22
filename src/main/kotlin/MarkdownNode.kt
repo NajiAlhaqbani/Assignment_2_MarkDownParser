137a993065c300a7d6f0af4e5b0e039c81e24e21
NajiAlhaqbani <91154392+NajiAlhaqbani@users.noreply.github.com>
2021-09-22 14:17:23 -0400

    MarkdownNode Updated
    for the regular text check in the if statement, Im not sure if we need <text> </text> or no.

--- src/main/kotlin/MarkdownNode.kt
+++ src/main/kotlin/MarkdownNode.kt
@@ -1,10 +1,22 @@
 class MarkdownNode : Node() {
     var type:Int = 0
-    override fun toHTML(): String {
-        TODO("Just check the type and return the html tag and the text string")
-        if(type==1)
-        {
-            //return "<h1>${this.text} </h1>"
+    override fun toHTML() : String {
+        if (type == 1) {
+            return ("<h1>${this.text}</h1>")
+        } else if (type == 2) {
+            return ("<h2>${this.text}</h2>")
+        } else if (type == 3) {
+            return ("<h3>${this.text}</h3>")
+        } else if (type == 4) {
+            return ("${this.text}")
+        } else if (type == 5) {
+            return ("<b>${this.text}</b>")
+        } else if (type == 6) {
+            return ("<i>${this.text}</i>")
+        } else if (type == 7) {
+            return ("<blockquote>${this.text}</blockquote>")
         }
+
+        return this.text
     }
 }
